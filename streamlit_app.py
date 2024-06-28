3dfb72d34ad3e5f0abc6fbb6571c784a1cb7f318
amerycano <merysitumorang.business@gmail.com>
2024-06-28 11:19:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ def main():
     st.write('Some Pythagorean Equation:')
     st.latex('c^2 = a^2 + b^2')
     st.dataframe(house) #menampilkan dataframe
-
+    st.write('Metrics')
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 if __name__ == '__main__':
     main()
 
