0ff7f1ffc7f4215f7101939ddcdc7f0cc85ad49c
amerycano <merysitumorang.business@gmail.com>
2024-06-28 12:55:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+from st_aggrid import AgGrid
 # Load the CSV file
 house = pd.read_csv('house_clean.csv')
 
@@ -11,6 +11,8 @@ def main():
     st.write('Some Pythagorean Equation:')
     st.latex('c^2 = a^2 + b^2')
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+    st.write('Displaying Data using AgGrid:')
+    AgGrid(house)  # Use AgGrid to display the DataFrame
 
 if __name__ == '__main__':
     main()
