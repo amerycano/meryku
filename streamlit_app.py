f9054670598a538b084e952b0903bbbffa0a3979
amerycano <merysitumorang.business@gmail.com>
2024-06-28 14:35:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,5 +75,13 @@ def main ():
          st.write("Angka Dipilih", slider_val, "checkbox", checkbox_val)
 
   st.write("Area di Luar Form")
+  # Insert containers separated into tabs:
+  tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+  tab1.write("this is tab 1")
+  tab2.write("this is tab 2")
+
+  # You can also use "with" notation:
+  with tab1:
+     st.radio("Select one:", [1, 2])
 if __name__ == '__main__':
     main()
