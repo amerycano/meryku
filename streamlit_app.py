a52e5ffb90c4d12acf52eca72c75eb6b29b5ecbf
amerycano <merysitumorang.business@gmail.com>
2024-06-28 12:57:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,12 @@ import pandas as pd
 
 house = pd.read_csv('house_clean.csv')
 
-def main():
-    st.header('This is Header')
-    st.subheader('This is SubHeader')
-    st.markdown('# Rendering Markdown')
-    st.write('Some Pythagorean Equation:')
-    st.latex('c^2 = a^2 + b^2')
-    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+def main() : 
+  st.header('This is Header')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__' : 
+  main()
