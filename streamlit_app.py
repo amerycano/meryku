cdaff1fb944a2882cec44ff199b8fc30a43f06e2
amerycano <merysitumorang.business@gmail.com>
2024-06-28 11:10:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,18 @@
-import streamlit as st 
-import pandas as pd 
-import requests
-from st_aggrid import AgGrid
+import streamlit as st
+import pandas as pd
 
-house  = pd.read_scv('house_clean.csv')
-                     
-def main() : 
-  st.write('Minimal Example')
+# Load the CSV file
+house = pd.read_csv('house_clean.csv')
 
-if __name__ == '__main__' : 
-  main()
-def main() : 
-  st.header('Halaman Streamlit Mery')
-  st.subheader('This is SubHeader')
-  st.markdown('# Rendering Markdown ')
-  st.write('Some Phytagorean Equation : ')
-  st.latex('c^2 = a^2+b^2')
-  st.dataframe(house)
+def main():
+    st.header('Halaman Streamlit Mery')
+    st.subheader('This is SubHeader')
+    st.markdown('# Rendering Markdown')
+    st.write('Some Pythagorean Equation:')
+    st.latex('c^2 = a^2 + b^2')
+    st.dataframe(house)
 
-if __name__ == '__main__' : 
-  main()
-
-
-
+if __name__ == '__main__':
+    main()
 
 
