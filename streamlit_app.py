30dde61f239b7c7cd3a453a481a41289b449970c
amerycano <merysitumorang.business@gmail.com>
2024-06-28 11:33:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid  # Import AgGrid from st_aggrid
 
 # Load the CSV file
 house = pd.read_csv('house_clean.csv')
 
 def main():
-  st.header('This is Header')
-  st.subheader('This is SubHeader')
-  st.markdown('# Rendering Markdown ')
-  st.write('Some Phytagorean Equation : ')
-  st.latex('c^2 = a^2+b^2')
-  st.write('Metrics:')
-  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+    st.header('This is Header')
+    st.subheader('This is SubHeader')
+    st.markdown('# Rendering Markdown')
+    st.write('Some Pythagorean Equation:')
+    st.latex('c^2 = a^2 + b^2')
+
+    st.write('Metrics:')
+    st.write('Temperature: 70 °F')
+    st.write('Delta: 1.2 °F')
 
 if __name__ == '__main__':
     main()
