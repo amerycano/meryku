1f402b7c730933fc0cf60f9e479499ef18c2c60b
amerycano <merysitumorang.business@gmail.com>
2024-06-28 10:53:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ def main() :
 if __name__ == '__main__' : 
   main()
 def main() : 
-  st.header('This is Header')
+  st.header('Halaman Streamlit Mery')
   st.subheader('This is SubHeader')
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
