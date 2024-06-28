f3e83234ce4587d7ea8b02f062fb9dbec97afe5e
amerycano <merysitumorang.business@gmail.com>
2024-06-28 11:29:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,18 @@ def main():
     st.markdown('# Rendering Markdown')
     st.write('Some Pythagorean Equation:')
     st.latex('c^2 = a^2 + b^2')
-    st.dataframe(house) #menampilkan dataframe
-    st.write('Metrics')
+    
+    # Display DataFrame using st.dataframe()
+    st.write('Displaying DataFrame:')
+    st.dataframe(house)
+
+    # Display metrics using st.metric()
+    st.write('Metrics:')
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-    st.write('Menampilkan Data House pada AgGrid')
-    AgGrid(house)
+
+    # Display DataFrame using AgGrid
+    st.write('Displaying Data using AgGrid:')
+    AgGrid(house)  # Use AgGrid to display the DataFrame
+
 if __name__ == '__main__':
     main()
-
-
