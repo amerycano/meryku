34e8074d20db16aa0e572c39a580bf9dcd9faa8e
amerycano <merysitumorang.business@gmail.com>
2024-06-28 13:14:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ def main() :
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
-
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  AgGrid(house)
+  st.table([x for x in range(1,5)])
 if __name__ == '__main__' : 
   main()
