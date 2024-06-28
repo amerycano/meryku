50868239bc135e03b3865e954a2c14e678bdf45f
amerycano <merysitumorang.business@gmail.com>
2024-06-28 13:44:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ def main ():
 
   st.write('Metrics')
   st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
-  st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(house)
-  st.table([x for x in range(1,5)])
+  # st.write('Menampilkan Dataframe dengan St AgGrid')
+  # AgGrid(house)
+  # st.table([x for x in range(1,5)])
   
    #create button
   click_me_btn = st.button('Click Me')
