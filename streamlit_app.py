c753435cc697b34c70ea95d61297457e0584f4dc
amerycano <merysitumorang.business@gmail.com>
2024-06-28 13:34:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,13 @@ def main ():
   AgGrid(house)
   st.table([x for x in range(1,5)])
   
-def main() : 
+##create button
     click_me_btn = st.button('Click Me')
     st.write(click_me_btn) #Return True kalo di Click 
     check_btn = st.checkbox('Klik Jika Setuju')
     if check_btn :
         st.write('Anda Setuju')
-    
-    
+        
     radio_button= st.radio('Choose below',[x for x in range(1,3)])
     st.write('Anda Memilih',radio_button)
     
