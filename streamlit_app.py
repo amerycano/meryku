cf9c58685687b5f59557ff2aa1361b0c43c52de5
amerycano <merysitumorang.business@gmail.com>
2024-06-28 10:01:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ def main() :
 
 if __name__ == '__main__' : 
   main()
+def main() : 
+  st.header('This is Header')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
+
+if __name__ == '__main__' : 
+  main()
