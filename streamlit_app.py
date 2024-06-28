737c2d536856f588f4b65cd5f5007a80ed2e7b17
amerycano <merysitumorang.business@gmail.com>
2024-06-28 13:15:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pandas as pd
+# import requests
+from st_aggrid import AgGrid
+
 house = pd.read_csv('house_clean.csv')
 
-def main() : 
+def main ():
   st.header('This is Header')
   st.subheader('This is SubHeader')
   st.markdown('# Rendering Markdown ')
-  st.write('Some Phytagorean Equation : ')
+  st.write('Some Pythagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
+
+  st.dataframe(house)
+
+  st.write('Metrics')
+  st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
   st.write('Menampilkan Dataframe dengan St AgGrid')
   AgGrid(house)
   st.table([x for x in range(1,5)])
-if __name__ == '__main__' : 
-  main()
+if _name_ == '_main_' :
+  main()
